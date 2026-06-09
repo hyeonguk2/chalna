5d3775c309b06a9d6892faddf9d53b70bb67bd3f
hyeonguk2 <80465980+hyeonguk2@users.noreply.github.com>
2026-06-09 16:50:19 +0900

    Revert "영상 캡차 업데이트"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -45,7 +44,6 @@ db.connect((err) => {
     });
 });
 
-// 회원가입
 app.post("/signup", (req, res) => {
     const { userid, email, password } = req.body;
 
@@ -53,7 +51,6 @@ app.post("/signup", (req, res) => {
         "INSERT INTO users (userid, password, email) VALUES (?, ?, ?)";
 
     db.query(sql, [userid, password, email], (err, result) => {
-
         if (err && err.code === "ER_DUP_ENTRY") {
             return res.status(409).send("duplicate");
         }
@@ -67,21 +64,6 @@ app.post("/signup", (req, res) => {
     });
 });
 
-app.use(express.json());
-// 라우터 연결
-app.use("/ocrcaptcha", require("./routes/ocrcaptcha"));
-
-app.use(
-    "/videos",
-    express.static(path.join(__dirname, "public/videos"))
-);
-
-app.use("/mvcaptcha", require("./routes/mvcaptcha"));
-
-const cleanupCaptcha = require("./routes/cleanupCaptcha");
-cleanupCaptcha(db);
-
-// 서버 시작
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
